6d2f705c2589791543a8e58bae84b5183034e065
prathamm-k <kairamkondapratham@gmail.com>
2024-09-16 19:01:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,8 +19,10 @@ st.markdown("Select an image file to analyze.")
 # displays file upload widget
 uploaded_file = st.file_uploader("Choose image file", type=['png', 'jpg', 'jpeg'] )
 
-
-image_model = 'llava:13b'
+# sets up sidebar nav widgets
+#with st.sidebar:   
+    # creates selectbox to pick the model we would like to use
+image_model = 'llava:7b'
 
 if chat_input := st.chat_input("What would you like to ask?"):
     if uploaded_file is None:
